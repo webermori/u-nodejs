dd699db35452d3b534d539740e5df05d5a3dc181
Weber Mori <webermori@gmail.com>
2018-11-10 15:53:14 -0200

    Módulo EJS

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/', function(req,res){
-	res.send("<html><body>Portal de Notícias</body></html>");
+	res.render("home/index");
 });
 
-app.get('/moda', function(req,res){
-	res.send("<html><body>Notícias de Moda</body></html>");
+app.get('/formulario_inclusao_noticias', function(req,res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req,res){
+	res.render("noticias/noticias");
 });
 
 app.listen(3000, function(){
