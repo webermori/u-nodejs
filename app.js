e1074ed5853d11b96f5dc127e5d53569edb8bb7a
Weber Mori <webermori@gmail.com>
2018-11-10 19:49:22 -0200

    Rotas

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine','ejs');
-
-app.get('/', function(req,res){
-	res.render("home/index");
-});
-
-app.get('/formulario_inclusao_noticias', function(req,res){
-	res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req,res){
-	res.render("noticias/noticias");
-});
+var app = require('./config/server');
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias')(app);
 
 app.listen(3000, function(){
 	console.log("Its Work!");
---
+++ mod_teste.js
@@ -0,0 +1,4 @@
+module.exports = function(){
+	var msg = "Modulo contém apenas uma string";
+	return msg;
+};
